d6693128a65b90def511f0614093291d05a3a06b
Yoann <yoann.deblois@gmail.com>
2022-01-05 20:31:42 +0100

    tests: first commit

---
+++ tests/utils.py
@@ -0,0 +1,38 @@
+def mock_load_Clubs_or_Competitions_empty(self):
+    return {}
+
+
+def mock_loadClubs_not_empty(self):
+    clubs = [
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "13"
+        },
+        {
+            "name": "Iron Temple",
+            "email": "admin@irontemple.com",
+            "points": "4"
+        },
+        {
+            "name": "She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points": "12"
+        }]
+    return clubs
+
+
+def mock_loadCompetitions_not_empty(self):
+    competitions = [
+        {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        }
+    ]
+    return competitions
