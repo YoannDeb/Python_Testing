64cc9c8b7db569c150889a42cffe4a35115251f3
Yoann <yoann.deblois@gmail.com>
2022-01-08 04:06:36 +0100

    tests: some prototype code for mocking added and commented for later.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 
 from server import create_app
+from tests.utils import mock_loadClubs_not_empty, mock_loadCompetitions_not_empty
 
 
 @pytest.fixture
-def client():
+def client(mocker):
     app = create_app({"TESTING": True})
+    # mocker.patch('server.loadClubs', return_value=mock_loadClubs_not_empty())
+    # mocker.patch('server.loadCompetitions', return_value=mock_loadCompetitions_not_empty())
+    # app.clubs = mock_loadClubs_not_empty()
+    # app.competitions = mock_loadCompetitions_not_empty()
     with app.test_client() as client:
         yield client
