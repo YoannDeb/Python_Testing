787f5671a64bd67d9c3b1301248e465838ad45ca
Yoann <yoann.deblois@gmail.com>
2022-01-14 16:50:46 +0100

    func: changed form email field label

--- forms/forms.py
+++ forms/forms.py
@@ -2,4 +2,4 @@ from wtforms import Form, StringField, validators
 
 
 class RegistrationForm(Form):
-    email = StringField('email', [validators.InputRequired(), validators.Length(max=100), validators.Email()])
+    email = StringField('Email:', [validators.InputRequired(), validators.Length(max=100), validators.Email()])
