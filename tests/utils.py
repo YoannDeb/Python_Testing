5aa5b9923f4dd9089c8c861590b48ab185805650
Yoann <yoann.deblois@gmail.com>
2022-01-14 17:40:06 +0100

    tests: changed dates in data mockup to have only future competitions except one.

--- tests/utils.py
+++ tests/utils.py
@@ -30,14 +30,19 @@ def mock_loadCompetitions_not_empty():
     competitions = [
         {
             "name": "Spring Festival",
-            "date": "2020-03-27 10:00:00",
+            "date": "2029-03-27 10:00:00",
             "numberOfPlaces": "25"
         },
         {
             "name": "Fall Classic",
-            "date": "2020-10-22 13:30:00",
+            "date": "2029-10-22 13:30:00",
             "numberOfPlaces": "13"
-        }
+        },
+        {
+            "name": "Past competition",
+            "date": "2020-05-07 13:45:00",
+            "numberOfPlaces": "42"
+        },
     ]
     return competitions
 
