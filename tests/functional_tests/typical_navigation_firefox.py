3911457da0b07f3f428796376b574bca1cffddab
Yoann <yoann.deblois@gmail.com>
2022-01-26 20:42:15 +0100

    tests: changed date of first competition to be a future competition

--- tests/functional_tests/typical_navigation_firefox.py
+++ tests/functional_tests/typical_navigation_firefox.py
@@ -1,56 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
-# from flask_testing import LiveServerTestCase
 import time
 
-# from server import app
-# from tests import conftest
-# from tests.functional_tests import config
 
-# PATH = r"C:\Program Files (x86)\chromedriver.exe"
-
-
-def setup_chrome_webdriver():
-    service = Service(executable_path=ChromeDriverManager().install())
-    options = ChromeOptions()
+def setup_firefox_webdriver():
+    service = Service(executable_path=GeckoDriverManager().install())
+    options = FirefoxOptions()
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 
-# class TestLogin(LiveServerTestCase):
-#
-#     def create_app(self):
-#         app.config.from_object('tests.functional_tests.config')
-#         return app
-#
-#     def setUp(self):
-#         self.driver = webdriver.Chrome(PATH)
-#
-#     def tearDown(self):
-#         self.driver.quit()
-#
-#     def test_open_chrome_windows(self):
-#         self.browser = webdriver.Chrome('tests/functional_tests/chromedriver.exe')
-#         self.browser.get(self.get_server_url())
-#         time.sleep(30)
-#         self.browser.close()
-#     #     self.driver.get(self.get_server_url())
-#     #     assert self.driver.current_url == 'http://localhost:8943/'
-
-# def test_open_index():
-#     driver = setup_chrome_webdriver()
-#     driver.get("http://127.0.0.1:5000")
-#     assert driver.title == "GUDLFT Registration"
-#     # driver.implicitly_wait(1000)
-#     time.sleep(10)
-#     driver.quit()
-
-
 def test_typical_navigation():
-    driver = setup_chrome_webdriver()
+    driver = setup_firefox_webdriver()
     driver.maximize_window()
     driver.get('http://127.0.0.1:5000')
     time.sleep(5)
@@ -87,7 +51,4 @@ def test_typical_navigation():
     time.sleep(5)
     logout_link = driver.find_element(By.LINK_TEXT, 'Logout')
     logout_link.click()
-    time.sleep(5)
     driver.quit()
-
-
