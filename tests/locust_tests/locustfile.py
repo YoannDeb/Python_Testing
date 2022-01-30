8eca50d489d3808e2fcb487489761a64ea5ff874
Yoann <yoann.deblois@gmail.com>
2022-01-30 23:37:04 +0100

    docs: Comments done.

--- tests/locust_tests/locustfile.py
+++ tests/locust_tests/locustfile.py
@@ -4,20 +4,35 @@ from locust import HttpUser, task
 class GUDLFT(HttpUser):
     @task
     def index(self):
+        """
+        Locust's task for index page.
+        """
         self.client.get("/")
 
     @task
     def pointsDisplay(self):
+        """
+        Locust's task for pointsDisplay page.
+        """
         self.client.get("/pointsDisplay")
 
     @task
     def showSummary(self):
+        """
+        Locust's task for showSummary page.
+        """
         self.client.post("/showSummary", data={'email': 'john@simplylift.co'})
 
     @task
     def book(self):
+        """
+        Locust's task for book page.
+        """
         self.client.get("/book/Spring%20Festival/Simply%20Lift")
 
     @task
     def purchasePlaces(self):
+        """
+        Locust's task for purchasePlaces page.
+        """
         self.client.post("/purchasePlaces", data={'club': 'Simply Lift', 'competition': 'Spring Festival', 'places': '1'})
